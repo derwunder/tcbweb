6c161adebcd10aedf67ed0fd276b79b2138fb26e
Christian Fontes <christianfontes82@gmail.com>
2017-10-27 15:03:34 -0400

    Package Npm
    all the config for the packages

--- src/Components/home/test.js
+++ src/Components/home/test.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Logo, HomeCss } from './../../directory';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={Logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Component desde <code>src/Components/home/test.js</code> and save to reload.
         </p>
       </div>
     );
---
+++ src/Views/Home.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import { TestHome, HomeCss } from './../directory';
+
+export class Home extends Component {
+  render() {
+    return <TestHome />
+  }
+}
