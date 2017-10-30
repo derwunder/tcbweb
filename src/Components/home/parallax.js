96a5399f60d0acb293476a8853140be95aa87893
Christian Fontes <christianfontes82@gmail.com>
2017-10-30 13:25:14 -0400

    Testing parallax

--- src/Components/home/parallax.js
+++ src/Components/home/parallax.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import Parallax from 'react-springy-parallax'
 import { Logo, HomeCss } from './../../directory';
-
+import Background from './../../Images/bg.jpg';
+import Background2 from './../../Images/bg2.jpg';
+import Background3 from './../../Images/bg3.jpg';
 class ParallaxComponent extends Component {
   render() {
     const styles = {
@@ -13,9 +15,9 @@ class ParallaxComponent extends Component {
     }
     return (
         <Parallax ref="parallax" pages={3}>
-            <Parallax.Layer offset={0} speed={1} style={{ backgroundColor: '#243B4A' }} />
-            <Parallax.Layer offset={1} speed={1} style={{ backgroundColor: '#805E73' }} />
-            <Parallax.Layer offset={2} speed={1} style={{ backgroundColor: '#87BCDE' }} />
+            <Parallax.Layer offset={0} speed={-0.2} style={{ backgroundImage: `url(${Background})` }} />
+            <Parallax.Layer offset={1} speed={-0.5} style={{ backgroundImage: `url(${Background2})` }} />
+            <Parallax.Layer offset={2} speed={-0.3} style={{ backgroundImage: `url(${Background3})` }} />
 
             <Parallax.Layer
                 offset={0}
@@ -26,14 +28,14 @@ class ParallaxComponent extends Component {
             </Parallax.Layer>
             <Parallax.Layer
                 offset={1}
-                speed={-0.1}
+                speed={0.8}
                 style={styles}
                 onClick={() => this.refs.parallax.scrollTo(2)}>
                 Another page ...
             </Parallax.Layer>
             <Parallax.Layer
                 offset={2}
-                speed={0.5}
+                speed={-0.5}
                 style={styles}
                 onClick={() => this.refs.parallax.scrollTo(0)}>
                 The end.
